dabaa988f9859a85cb0268622fb650abcac97d33
Basemx462 <basemx462@gmail.com>
2023-08-31 02:22:49 +0300

    NEW2

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
 
   cloud {
-    organization = "devops-project-org"
+    organization = "Bassem-Org"
 
     workspaces {
-      name = "devops-project-workspace"
+      name = "project2"
     }
   }
 }
